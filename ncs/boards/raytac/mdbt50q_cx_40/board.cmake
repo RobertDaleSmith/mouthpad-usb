8fbba51b36132cd12d6bbc427c0c29136a55f4d9
Robert Dale Smith <robert@robertdalesmith.com>
2025-11-03 22:16:42 -0600

    Move CX-40 board definition to ncs/app/boards and use BOARD_ROOT
    - Consolidate custom board in ncs/app/boards/raytac/mdbt50q_cx_40/
    - Remove duplicate ncs/boards/ directory
    - Update CI to pass -DBOARD_ROOT to find custom boards
    - All board files (definitions, conf, overlay) now in one location
    - No modifications to NCS SDK dependencies
    - Works locally and in CI with -DBOARD_ROOT cmake argument

--- ncs/boards/raytac/mdbt50q_cx_40/board.cmake
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2018-2023 Nordic Semiconductor ASA
-# SPDX-License-Identifier: Apache-2.0
-
-# Board uses Nordic DFU bootloader
-# Flashing is done via nrfutil outside of the build system
